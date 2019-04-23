aeb72123a420c3ca4b46ceb814938dcf6b7a1889
HaneenWally <megemen2020@gmail.com>
2019-04-23 18:36:23 +0200

    Update Files

--- IO/IOInterface.vhd
+++ IO/IOInterface.vhd
@@ -1,14 +1,32 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-entity IOInterface is
-    Port(clk: in std_logic;
-        rst: in std_logic;
-        din : in std_logic_vector(15 downto 0));
-end entity IOInterface;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
 
-architecture ArchIOInterface of IOInterface is
-begin
-   
-end ArchIOInterface;
+ENTITY IOInterface IS
+	PORT(
+		i_clk	:	IN	std_logic;
+		i_rst	:	IN	std_logic;
+		i_int	:	IN	std_logic;
+		i_load	:	IN	std_logic;
+		i_data	:	IN	std_logic_vector(15 downto 0);
+		o_data	:	OUT	std_logic_vector(15 downto 0)
+	);
+END ENTITY;
 
+ARCHITECTURE behavioral OF IOInterface IS
 
+COMPONENT nReg IS
+GENERIC (n	:	integer:=16);
+	PORT (
+		input		:	IN	std_logic_vector(n-1 downto 0);
+		en,rst,clk	:	IN	std_logic;
+		output		:	OUT	std_logic_vector(n-1 downto 0));
+END COMPONENT;
+
+SIGNAL enReg	:	std_logic;
+
+BEGIN
+
+	packetReg:	nReg GENERIC MAP(16) PORT MAP(i_data, enReg, i_rst, i_clk, o_data);
+	enReg <= i_int and i_load;
+
+END ARCHITECTURE;
