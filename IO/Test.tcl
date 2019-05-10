9816958f58540f64cd3c1ed6c197748fca6f01c3
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-05-10 19:38:28 +0200

    adjusted tcl script [Now Image is read right]

--- IO/Test.tcl
+++ IO/Test.tcl
@@ -1,5 +1,6 @@
 proc c  {} {
     vcom integration.vhd   
+    # vcom *.vhd 
     echo "---------------COMPILATION IS OVER--------------------------"
     vsim work.accelerator
     echo "---------------STARTED SIMULATION---------------------------"
@@ -18,11 +19,10 @@ proc bitsToHex bits {
      return $hex
 }
 
-set fp [open "compimg.txt" r]
+set fp [open "img.txt" r]
 set file_data [read $fp]
 close $fp
 set sz [string  length $file_data]
-
 #  compile, start sim and rst
 c
 
@@ -40,45 +40,59 @@ while {$R==0} {
 run
 force -freeze sim:/accelerator/i_interrupt 1 0
 run
-force -freeze sim:/accelerator/i_interrupt 0 0
+# force -freeze sim:/accelerator/i_interrupt 0 0
 
-set k 0
 set j 0
-for { set i 0 }  {$i < $sz} {set i [expr {$i + 15}]} {
+for { set i 0 }  {$i < $sz} {set i [expr {$i + 16}]} {
     set arr($j) [string range $file_data $i $i+15]
     puts "$arr($j)"
     set hex [bitsToHex $arr($j)]
     puts $hex
     force -freeze sim:/accelerator/i_din $hex 0    
     run
+    force -freeze sim:/accelerator/i_interrupt 0 0
     incr j
-    # puts{"Image"}
+    puts $j
     set x [examine o_ready]
-    puts $x
+    # puts $x
     while {$x==0} {
-        incr k
         run
         set x [examine o_ready]
-        puts "Image"
-        if { $k> 20} {
-            exit 1 
-            # force exit for now
-        }
     }
+    force -freeze sim:/accelerator/i_interrupt 1 0
 }
 
 # wait for Done Signal
+force -freeze sim:/accelerator/i_interrupt 0 0
+# run
+set D [examine o_ready]
+while {$D==0} {
+    run
+    puts $D
+    set D [examine o_ready]
+}
+run
+####### image is done, So start sending CNN
+
+
+
+
+
+
+
+
+
+
+
+
+
+# after sending process to the chip wait for done
 
 # set D [examine o_done]
 # while {$D==0} {
 #     run
 #     set D [examine o_done]
 # }
-
-
-
-####### image is done, So start sending CNN
-
 
 
 
