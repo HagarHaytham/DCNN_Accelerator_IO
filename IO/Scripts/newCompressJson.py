bcc5aa4fa745a1383c34d059c293cee2126be343
HaneenWally <livetour1d>
2019-05-11 22:17:28 +0200

    Complete weights in compressed file

--- IO/Scripts/newCompressJson.py
+++ IO/Scripts/newCompressJson.py
@@ -10,15 +10,16 @@ with open('CNN_info_Sample.json') as f:
 layers = len(data['layers'])
 print('number of layers ', layers)
 unCompressed = str(np.binary_repr(layers, 16))
-print('unCompressed data after adding layers number ', unCompressed)
-#unCompressed += "\n"
+print('after adding layers number,', 'number of layers', layers, 'binary_repr', str(np.binary_repr(layers, 16)))
+# unCompressed += "\n"
 ########################################################################################################
 
 
 #used with fc to calculate number of channels
 out_sx_of_previous = list()
+
 #exponent, the amount of shift for weights and biases, to convert them to integer
-exponent = 8
+exponent = 14
 
 for i in range(layers):
 
@@ -36,7 +37,7 @@ for i in range(layers):
 
     #write layer type to unCompressed data
     unCompressed += str(np.binary_repr(layer_type, 16))
-    print('unCompressed after layer type', unCompressed)
+    print('after layer type,', 'type', layer_type, 'binary_repr', str(np.binary_repr(layer_type, 16)))
     #unCompressed += "\n"
 
     #get filter size
@@ -54,7 +55,7 @@ for i in range(layers):
 
     #write filter size to unCompressed data
     unCompressed += str(np.binary_repr(filter_size, 16))
-    print('unCompressed after filter size', unCompressed)
+    print('after filter size,', filter_size, 'binary_repr', str(np.binary_repr(filter_size, 16)))
     #unCompressed += "\n"
 
 
@@ -66,12 +67,12 @@ for i in range(layers):
 
     #write new layer size to unCompressed data
     unCompressed += str(np.binary_repr(new_layer_size, 16))
-    print('unCompressed after new layer size', unCompressed)
+    print('after new layer size,', 'new_layer_size', new_layer_size, 'binary_repr', str(np.binary_repr(new_layer_size, 16)))
     #unCompressed += "\n"
 
 	#write new layer size * new layer size to unCompressed data
     unCompressed += str(np.binary_repr(new_layer_size * new_layer_size, 16))
-    print('unCompressed after new layer size', unCompressed)
+    print('after new layer size^2,', 'new_layer_size^2', new_layer_size*new_layer_size, 'binary_repr', str(np.binary_repr(new_layer_size * new_layer_size, 16)))
     #unCompressed += "\n"    
 
 
@@ -83,48 +84,65 @@ for i in range(layers):
 
     #write new layer size * new layer size * filters in current layer to unCompressed data
     unCompressed += str(np.binary_repr(new_layer_size * new_layer_size * filters , 16))
-    print('unCompressed after new layer size', unCompressed)
+    print('after new layer size^2*filters,', 'new_layer_size^2*filters', new_layer_size*new_layer_size*filters, 'binary_repr', str(np.binary_repr(new_layer_size * new_layer_size * filters , 16)))
     #unCompressed += "\n"
 
 
     if layer_type == 2:
     	n_in_channels = np.math.floor(out_sx_of_previous[i-1] * out_sx_of_previous[i-1] / 25.0)
     	unCompressed += str(np.binary_repr(new_layer_size * new_layer_size * n_in_channels , 16))
-    	print('unCompressed after channels in fc', unCompressed)
-    	#unCompressed += "\n"
+    	print('after channels in fc,', 'new_layer_size*new_layer_size*nChannels', new_layer_size*new_layer_size*n_in_channels, 'binary_repr', str(np.binary_repr(new_layer_size * new_layer_size * n_in_channels , 16)))
+    	print('n_in_channels', n_in_channels)
+        #unCompressed += "\n"
 
     #write bias before filter
-    # if layer_type != 1:
-    # 	for f in range(layer['filters']):
-    # 		bias = layer['biases']
-    #         bias 
-    # 		unCompressed += str(np.binary_repr(bias, 16))
-    # 		print('unCompressed after bias', unCompressed)
-    # 		#unCompressed += "\n"
+    if layer_type != 1:
+        print('number of filters in layer,', len(layer['filters']))
+        for f in range(len(layer['filters'])):
+            bias = layer['biases']['w'][str(f)]
+            bias = int(bias * (2**exponent))
+            unCompressed += str(np.binary_repr(bias, 16))
+            print('after bias,', 'bias after multiplying by exponent', bias, 'binary_repr', str(np.binary_repr(bias, 16)))
+    		#unCompressed += "\n"
 
-    # 		for d in range(layer['filters']['depth']):
+            if layer_type == 0:
+                depth = new_layer_size
+            elif layer_type == 2:
+                depth = int(layer['num_inputs']/25.0)*25
+
+            print('depth of layer, of type', layer_type, 'depth', depth)
+                
+            print('depth', depth)
+
+            for d in range(depth):
+                weight = layer['filters'][f]['w'][str(d)]
+                weight = int(weight * (2**exponent))
+                unCompressed += str(np.binary_repr(weight, 16))
+                # print('after weight,', 'weight after multiplying by exponent', weight, 'binary_repr', str(np.binary_repr(weight, 16)))
+                # unCompressed += "\n"
     			
 
-    			
+print('\n\n')
 
 
 
-
-
-count = 0
+count = 1
 prev_bit = unCompressed[0]
 compressed = str()
 
-for i in range(len(unCompressed)):
-	if prev_bit == unCompressed[i] and count < 128:
-		count += 1
-		print('i', unCompressed[i], 'count', count)
-	else:
-		compressed += (prev_bit + str(np.binary_repr(count, 7)))
-		prev_bit = unCompressed[i]
-		count = 1
 
-print(compressed)
+for i in range(1, len(unCompressed)):
+    if prev_bit == unCompressed[i] and count < 127:
+        count += 1
+
+    else:
+        compressed += prev_bit
+        compressed += str(np.binary_repr(count, 7))
+        prev_bit = unCompressed[i]
+        print('prev_bit', prev_bit, 'count', count)
+        count = 1
+
+print(len(unCompressed))
 
 with open ("jsnUn.txt",'w') as f:            
     f.write(unCompressed)
