f5d29524c311474c36c0662f90b0e6b4266f8426
Mary_N <marynader256@gmail.com>
2019-05-11 15:25:57 +0200

    small bug solved

--- IO/JSONDecomp.vhd
+++ IO/JSONDecomp.vhd
@@ -127,9 +127,13 @@ if(rising_edge(clk)) then
 			elsif( LorR='1' and dnCount="0000001") then
 				bgn <= '0';
 				shRegEn <= '0';
-				upCEn <= '0';
+				--upCEn <= '0';
 				LorR <= '0';
 				
+			end if;
+
+			if(LorR = '1' and dnCount = "0000010") then
+				upCEn <= '0';
 			end if;
 
 
